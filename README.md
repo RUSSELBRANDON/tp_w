3a3cf187a53bd6c82db5e61bf1ef71950b0095e4
dongmo russel <russeldongmo96@gmail.com>
2023-05-24 08:56:17 +0200

    enregistrement

---
+++ README.md
@@ -0,0 +1,6 @@
+#### TP_WEB
+
+### Presentation du tp
+Ceci est un fichier que tout projets Github devrait avoir. On y insere les generalites sur le projet et les steps pour 
+demarrer le projet en locale.
+NB: les '#' permettent de representer les zones de titre
---
+++ database/connection.php
@@ -0,0 +1,22 @@
+<?php
+function connection(){
+    $_SETTINGS = parse_ini_file('./../settings.ini', true);
+
+
+    $db = new \PDO(
+        "mysql:hostname={$_SETTINGS['db']['host']};dbname={$_SETTINGS['db']['name']}",
+        $_SETTINGS['db']['user'],
+        $_SETTINGS['db']['pass'],
+        [
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
+        ]
+    );
+
+    if ($db != null)
+        return $db;
+    else
+        throw new PDOException('impossible de se connecter a votre base de donnees\n');
+}
+
+?>
