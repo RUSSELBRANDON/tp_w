c602d92b3884b6b92c1a0d8d826b2eff97bfd458
dongmo russel <russeldongmo96@gmail.com>
2023-05-26 19:09:23 +0200

    authentification

--- views/pages/inscription.php
+++ views/pages/inscription.php
@@ -1,36 +1,39 @@
+<fieldset style=" height:400px; margin:100px 200px 200px 200px ; background-color:orange"><a href="inscription.html" style="color:blue; font-size:40px; text-align:center; margin-top:250px;"> retourner a la page de connexion </a></fielsed>
 <?php
 
+require ' ./../../../database/connection.php';
 //Récupération des données du formulaire
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password1'];
 $password2 = $_POST['password2'];
-
+if($password!==$password2){
+?>  <script>alert('impossible de vous inscrire car le mot de passe est errone. Veillez recommencer votre inscription'); </script> <?php
+exit();
+}
 //Connexion à la base de données
-$host = "localhost";
-$dbname = "sweet_shop_db";
-$user = "root";
-$pass = "";
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db = connection();
 
-    //Préparation de la requête d'insertion
-    $date_enregist=date("d/m/y");
-    $ins = $pdo->prepare("INSERT INTO utilisateur (name_user, email, password,date_enregistrement) VALUES (:nom, :email, :password1, :dates)");
-    //Assignation des valeurs aux paramètres de la requête
-    $ins->bindParam(':nom', $name);
-    $ins->bindParam(':email', $email);
-    $ins->bindParam(':password1', $password);
-    $ins->bindParam(':dates', $date_enregist );
+ //Préparation de la requête d'insertion
+$date_enregist=date("d/m/y");
+$ins = $db->prepare("INSERT INTO utilisateur (name_user, email, mot_de_passe,date_enregistrement) VALUES (:nom, :email, :password1, :dates)");
+ //Assignation des valeurs aux paramètres de la requête
+ $ins->bindParam(':nom', $name);
+ $ins->bindParam(':email', $email);
+ $ins->bindParam(':password1', $password);
+ $ins->bindParam(':dates', $date_enregist );
 
  //Exécution de la requête
-if($ins) 
+ $ins->execute();
+
+if ($ins) 
 {
-	?><script>alert("Insertion Reussit");</script><?php
+	?><script>alert("Inscription approuvee");</script><?php
 }else
 {
-	?><script>alert("Echec de l'Insertion");</script><?php
+	?><script>alert("Echec de l'inscription");</script><?php
 }
 //Fermeture de la connexion à la base de données
 $pdo = null;
+
 ?>
