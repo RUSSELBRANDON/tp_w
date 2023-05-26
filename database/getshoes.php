7aa8259c4ac3203d822eeea97850eeace4a733f4
dongmo russel <russeldongmo96@gmail.com>
2023-05-26 22:42:27 +0200

    recuperation produit

--- database/getshoes.php
+++ database/getshoes.php
@@ -1,38 +1,26 @@
-<fieldset style=" height:400px; margin:100px 200px 200px 200px ; background-color:orange"><a href="inscription.html" style="color:blue; font-size:40px; text-align:center; margin-top:250px;"> retourner a la page de connexion </a></fielsed>
+<fieldset style=" height:400px; margin:100px 200px 200px 200px ; background-color:orange"></fielsed>
 <?php
 
-require 'connection.php';
-//Récupération des données du formulaire
-$search = $_POST['search'];
-
-//connection a la base de donnes
-$host = "localhost";
-$dbname = "sweet_shop";
-$user = "root";
-$pass = "";
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$rech = $pdo->query("SELECT reference FROM produit WHERE reference= $search LIMIT 1");
-//$rech->execute(array($search));
-if($rech="reebook" || $rech="REEBOOK"){
+$rech=$_POST['search'];
+if($rech==="reebook" || $rech==="REEBOOK"){
     ?>
-    <a href="reebook.php"> decouvrir les articles Reebook </a> 
+    <a href="reebook.php" style="font-size:40px;color:black"> decouvrir les articles Reebook </a> 
      <?php
-}else if($rech="le coq sportif" || $rech="LE COQ SPORTIF"){
+}else if($rech==="le coq sportif" || $rech==="LE COQ SPORTIF"){
     ?>
-    <a href="coq.php.php"> decouvrir les articles LE COQ SPORTIF </a> 
+    <a href="coq.php.php" style="font-size:40px;color:black"> decouvrir les articles LE COQ SPORTIF </a> 
      <?php
 }else
-if($rech="puma" || $rech="PUMA"){
+if($rech==="puma" || $rech==="PUMA"){
     ?>
-    <a href="puma.php"> decouvrir les articles PUMA </a> 
+    <a href="puma.php" style="font-size:40px;color:black"> decouvrir les articles PUMA </a> 
      <?php
 }else
-if($rech="TIMBERLAND" || $rech="TIMBERLAND"){
+if($rech==="TIMBERLAND" || $rech==="TIMBERLAND"){
     ?>
-    <a href="timberland.php"> decouvrir les articles TIMBERLAND </a> 
+    <a href="timberland.php" style="font-size:40px;color:black"> decouvrir les articles TIMBERLAND </a> 
      <?php
+}else{
+    ?> <h3 style="font-size:40px;color:black"> articles introuvable!!! verifier le nom et recommencer <?php
 }
-//fermeture de la base de donnees
-$pdo=null;
 ?>
