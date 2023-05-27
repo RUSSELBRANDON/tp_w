722de5a26eaa87fc7789c4083d4afad1e664bf56
Merritz <morritzngueguim43@gmail.com>
2023-05-27 21:12:10 +0200

    m3

--- views/pages/parts/nav.php
+++ views/pages/parts/nav.php
@@ -2,13 +2,13 @@
     <div id="entete1">
     <h1>SWEET SHOP</h1>
     <h5> faites vous plaisir chez nous</h5><br>
-    <form method="post" action="./../../database/getshoes.php">
+    <form method="post" action="http://localhost/database/getshoes.php">
         <input type="text" placeholder="rechercher un produit" name="search">
         <button type="submit">rechercher</button>
     </form>
     <nav>
         <ul>
-            <li> <a href="connexion.html"> COMPTE</a></li>
+            <li> <a href="connexion.php"> COMPTE</a></li>
             <b class="far fa-user"></b>
             <li style="font-size: 22px;"> PANIER</li>
         </ul>
@@ -17,10 +17,10 @@
     <div id="entete2">
         <nav>
             <ul>
-                <li> <a href="hommes.html"> HOMMES</a></li>
-                <li> <a href="femmes.html"> FEMMES</a></li>
-                <li> <a href="enfants.html"> ENFANTS</a></li>
-                <li> <a href="marques.html"> MARQUES</a></li>
+                <li> <a href="hommes.php"> HOMMES</a></li>
+                <li> <a href="femmes.php"> FEMMES</a></li>
+                <li> <a href="enfants.php"> ENFANTS</a></li>
+                <li> <a href="marques.php"> MARQUES</a></li>
             </ul>
         </nav>
     </div>
