7fa808c96cff2f6d3131b791589f0e989fc2a9c8
Merritz <morritzngueguim43@gmail.com>
2023-05-27 22:35:19 +0200

    m4

--- database/GetProducts.php
+++ database/GetProducts.php
@@ -2,7 +2,7 @@
     function getAllShoes($db)
     {
         $req = $db->prepare('SELECT * FROM produits');
-        $prods = $req->execute([]);
-        return $prods;
+        $req->execute([]);
+        return $req->fetchAll();
     }
 ?>
