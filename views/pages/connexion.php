81be76c7063b5d38bda10ed41860a43f6742648f
dongmo russel <russeldongmo96@gmail.com>
2023-05-27 07:20:52 +0200

    test validite

---
+++ views/pages/connexion.php
@@ -0,0 +1,24 @@
+<?php
+$email=$_POST['email'];
+$pass=$_POST['mot_de_passe']; 
+// connection a la base de donnees
+$host = "localhost";
+$dbname = "sweet_shop";
+$user = "root";
+$pass = "";
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$mail = $pdo->prepare("SELECT email FROM utilisateur WHERE email=?");
+$mail->execute(["$email"]);
+$password = $pdo->prepare("SELECT mot_de_passe FROM utilisateur WHERE mot_de_passe=?");
+$password->execute(["$pass"]);
+if($mail===null && $password===null){
+    echo " compte inexistant";
+}else if($mail===null){
+    echo " l'email entree ne correspond pas";
+}elseif( $password===null){
+    echo " le mot de passe entree ne correspond pas";
+}else{
+    echo " bienvenu ";
+}
+?>
